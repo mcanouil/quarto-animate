9ff910b32592e297e257448d0cbe6f85c9176381
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2026-09-06 18:33:34 +0200

    feat: declare the vendored modules in a manifest

--- _extensions/animate/animate-shortcodes.lua
+++ _extensions/animate/animate-shortcodes.lua
@@ -7,10 +7,10 @@
 local EXTENSION_NAME = 'animate'
 
 --- Load required modules
-local str = require(quarto.utils.resolve_path("_modules/string.lua"):gsub("%.lua$", ""))
-local log = require(quarto.utils.resolve_path("_modules/logging.lua"):gsub("%.lua$", ""))
-local meta_mod = require(quarto.utils.resolve_path("_modules/metadata.lua"):gsub("%.lua$", ""))
-local html_mod = require(quarto.utils.resolve_path("_modules/html.lua"):gsub("%.lua$", ""))
+local str = require(quarto.utils.resolve_path("_vendor/quarto-lua-modules/string.lua"):gsub("%.lua$", ""))
+local log = require(quarto.utils.resolve_path("_vendor/quarto-lua-modules/logging.lua"):gsub("%.lua$", ""))
+local meta_mod = require(quarto.utils.resolve_path("_vendor/quarto-lua-modules/metadata.lua"):gsub("%.lua$", ""))
+local html_mod = require(quarto.utils.resolve_path("_vendor/quarto-lua-modules/html.lua"):gsub("%.lua$", ""))
 local validation = require(quarto.utils.resolve_path("_modules/validation.lua"):gsub("%.lua$", ""))
 
 --- Array of supported animation effects from Animate.css library
